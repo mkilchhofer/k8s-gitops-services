bc3e1e2285602e0829ea969ac2cab309775b0003
Marco Kilchhofer <marco@kilchhofer.info>
2022-08-10 00:42:48 +0200

    chore: Manage App of Apps with TF

---
+++ k3s-bootstrap/argocd_apps.tf
@@ -0,0 +1,40 @@
+resource "argocd_application" "app_of_apps" {
+  metadata {
+    name      = "k8s-gitops-services"
+    namespace = "argocd"
+  }
+
+  spec {
+    project                = "base"
+    revision_history_limit = 0
+
+    source {
+      repo_url        = "git@github.com:mkilchhofer/k8s-gitops-services.git"
+      path            = "argocd"
+      target_revision = "main"
+      directory {
+        recurse = true
+      }
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "argocd"
+    }
+
+    sync_policy {
+      automated = {
+        prune       = false
+        self_heal   = false
+        allow_empty = false
+      }
+    }
+
+    info {
+      name  = "Owner"
+      value = "https://github.com/mkilchhofer/"
+    }
+
+  }
+  cascade = false
+}
