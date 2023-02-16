02be405d029083726d78834f517087db375a3635
Marco Kilchhofer <marco@kilchhofer.info>
2023-02-16 21:53:08 +0100

    chore: Update to Cilium 1.12.7

--- k3s-bootstrap/cilium.tf
+++ k3s-bootstrap/cilium.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cilium" {
   name       = "cilium"
   repository = "https://helm.cilium.io/"
   chart      = "cilium"
-  version    = "1.12.3"
+  version    = "1.12.7"
   namespace  = "kube-system"
 
   max_history = local.helm_max_history
