3ef0f504d2f9edd398e90743b2ff7917ef50e518
Marco Kilchhofer <marco@kilchhofer.info>
2023-07-10 00:19:16 +0200

    chore(ca-issuer): Migrate key-pair to Akeyless

--- k3s-bootstrap/cert-manager.tf
+++ k3s-bootstrap/cert-manager.tf
@@ -3,3 +3,23 @@ resource "kubernetes_namespace_v1" "cert_manager" {
     name = "cert-manager"
   }
 }
+
+data "akeyless_secret" "cert_manager_ca_issuer_tls_crt" {
+  path = "/k3s/cert-manager/ca-issuer_tls.crt"
+}
+
+data "akeyless_secret" "cert_manager_ca_issuer_tls_key" {
+  path = "/k3s/cert-manager/ca-issuer_tls.key"
+}
+
+resource "kubernetes_secret_v1" "cert_manager_ca_issuer_key_pair" {
+  metadata {
+    name      = "ca-issuer-key-pair"
+    namespace = kubernetes_namespace_v1.cert_manager.metadata.0.name
+  }
+
+  data = {
+    "tls.crt" = data.akeyless_secret.cert_manager_ca_issuer_tls_crt.value
+    "tls.key" = data.akeyless_secret.cert_manager_ca_issuer_tls_key.value
+  }
+}
