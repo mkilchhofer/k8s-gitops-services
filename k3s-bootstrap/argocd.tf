ec26aaea95666324c65c7b56a5ede3a6ad5acddf
Marco Maurer <marco@kilchhofer.info>
2023-11-15 20:57:57 +0100

    chore: Update to Argo CD 2.8.6

--- k3s-bootstrap/argocd.tf
+++ k3s-bootstrap/argocd.tf
@@ -32,7 +32,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "5.42.3"
+  version    = "5.50.1"
   namespace  = kubernetes_namespace.argocd.metadata.0.name
 
   max_history = local.helm_max_history
