f1365bf4ad0e95dd538e91c6a1747492c461c77a
Marco Maurer (-Kilchhofer) <mkilchhofer@users.noreply.github.com>
2023-12-07 00:14:02 +0100

    chore(akeyless): Fix deprecation warning (#14)

--- k3s-bootstrap/akeyless.tf
+++ k3s-bootstrap/akeyless.tf
@@ -5,13 +5,14 @@ resource "akeyless_auth_method_api_key" "k3s_eso" {
 resource "akeyless_role" "k3s_eso" {
   name = "k3s-eso"
 
-  assoc_auth_method {
-    am_name = akeyless_auth_method_api_key.k3s_eso.name
-  }
-
   rules {
     capability = ["read"]
     path       = "/k3s/*"
     rule_type  = "item-rule"
   }
+}
+
+resource "akeyless_associate_role_auth_method" "k3s_eso" {
+  am_name   = akeyless_auth_method_api_key.k3s_eso.name
+  role_name = akeyless_role.k3s_eso.name
 }
