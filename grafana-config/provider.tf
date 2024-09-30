a83804d98239c7ba7c6090bb591e785b837232f8
Marco Maurer <marco.kilchhofer@gmail.com>
2024-09-30 21:12:45 +0200

    chore(Akeyless): Migrate to EU region

--- grafana-config/provider.tf
+++ grafana-config/provider.tf
@@ -1,5 +1,5 @@
 provider "akeyless" {
-  api_gateway_address = "https://api.akeyless.io"
+  api_gateway_address = "https://api.eu.akeyless.io"
 
   api_key_login {
     access_id  = var.akeyless_access_id
