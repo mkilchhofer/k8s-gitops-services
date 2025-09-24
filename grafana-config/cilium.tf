943383266bdeca20e52f96d84b04b27bb461305d
Marco Maurer <marco.kilchhofer@gmail.com>
2025-09-24 15:44:30 +0200

    chore: Pull prometheus-alerts module via swisspost

--- grafana-config/cilium.tf
+++ grafana-config/cilium.tf
@@ -3,7 +3,8 @@ resource "grafana_folder" "cilium" {
 }
 
 module "cilium_alerts" {
-  source = "github.com/mkilchhofer/terraform-grafana-prometheus-alerts?ref=main"
+  source  = "swisspost/prometheus-alerts/grafana"
+  version = "~> 1.0"
 
   prometheus_alerts_file_path = file("./cilium/alerts.yaml")
   folder_uid                  = grafana_folder.cilium.uid
--- grafana-config/keda.tf
+++ grafana-config/keda.tf
@@ -3,7 +3,8 @@ resource "grafana_folder" "keda" {
 }
 
 module "keda_alerts" {
-  source = "github.com/mkilchhofer/terraform-grafana-prometheus-alerts?ref=main"
+  source  = "swisspost/prometheus-alerts/grafana"
+  version = "~> 1.0"
 
   prometheus_alerts_file_path = file("./keda/alerts.yaml")
   folder_uid                  = grafana_folder.keda.uid
